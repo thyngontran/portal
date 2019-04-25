1acbdd803b0f18f028504bd3fa0a2ec8e8c04850
ThyNgon Tran <thyngontran@gmail.com>
2019-04-25 09:51:11 -0400

    change non-production user pool in environment setting

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -7,17 +7,17 @@ export const environment = {
     production: false,
 
 
-    region: 'us-east-2',
+    region: 'us-east-1',
 
-    identityPoolId: 'us-east-2:898eb244-070d-4111-9b94-6aaf0ec4f761',
-    userPoolId: 'us-east-2_yIstCoKlH',
-    clientId: '66a84rgkfsbsc4pv8ifcvohgpk',
+    identityPoolId: 'us-east-1:b9479c45-530a-4cd2-b3ef-b2f583b4ef87',
+    userPoolId: 'us-east-1_ByrjN2T0t',
+    clientId: '6s2vkodb0haba30h8rskpe4452',
 
     rekognitionBucket: 'rekognition-pics',
     albumName: "usercontent",
     bucketRegion: 'us-east-2',
 
-    ddbTableName: 'LoginTrailcyburivbc',
+    ddbTableName: 'LoginTrailPortal',
 
     vbcTableName: 'CyburiPlayers',
 
